2f1f2a992731da3b4745ab586ccaf271545f3a5e
Joshua Wootonn <joshuawootonn@gmail.com>
2018-05-02 21:01:09 -0500

    Animationless about page finished

--- src/pages/index.md
+++ src/pages/index.md
@@ -2,8 +2,12 @@
 title: "Zach Rauch"
 type: index
 path: "/"
-left: "../images/about/about-1.jpg"
-right: "../images/about/about-2.jpg"
+row1Image: "../images/about/about-1.jpg"
+row1Title: "Zach Rauch"
+row1Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nulla justo. Nam et viverra eros."
+row21Image: "../images/about/about-2-1.jpg"
+row22Image: "../images/about/about-2-2.jpg"
+row2Title: "Zach's Life"
+row2Text : "incidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin congue nibh sit amet libero tempor, in semper dui gravida. "
 ---
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nulla justo. Nam et viverra eros. Vestibulum condimentum arcu sapien, quis mattis nulla volutpat in. Proin sodales eget leo at congue. Sed justo purus, sagittis sed purus id, tincidunt varius nunc. Sed hendrerit mi a eleifend consectetur. Donec non auctor nibh. Mauris eget ligula vitae elit viverra tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin congue nibh sit amet libero tempor, in semper dui gravida. Nunc vitae diam sagittis, dignissim magna nec, luctus magna. Pellentesque vitae ornare mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt diam non ipsum malesuada, eget pellentesque arcu molestie. Sed sed turpis magna.
--- src/templates/index.js
+++ src/templates/index.js
@@ -7,21 +7,48 @@ export default ({ data }) => {
   const aboutData = data.allMarkdownRemark.edges[0].node;
   console.log(aboutData);
   return (
-    <div className="columns">
-      <div className="column ">
-        <img
-          src={aboutData.frontmatter.left.publicURL}
-          className="about-image-1"
-        />
-        <img
-          src={aboutData.frontmatter.right.publicURL}
-          className="about-image-2"
-        />
+    <div className="container">
+      <div className="columns vertical-spacer">
+        <div className="column is-4 text-right text-center">
+          <div className="text-wrapper">
+            <p className="about-title">{aboutData.frontmatter.row1Title}</p>
+            <p className="about-text">{aboutData.frontmatter.row1Text}</p>
+          </div>
+        </div>
+        <div className="column relative height1 is-8">
+          <div className="about-image-wrapper-1">
+            <img
+              src={aboutData.frontmatter.row1Image.publicURL}
+              className="about-image"
+            />
+            </div>
+        </div>
       </div>
-      <div className="column">
-        <h1>{aboutData.frontmatter.title}</h1>
-        
-        <div dangerouslySetInnerHTML ={{__html: aboutData.html}} />
+      <div className="columns vertical-spacer">
+        <div className="column relative height2">
+          <div className="about-image-wrapper-2">
+            <div className="columns relative height2">
+              <div className="column">
+                <img
+                  src={aboutData.frontmatter.row21Image.publicURL}
+                  className="about-image"
+                />
+              </div>
+              <div className="column">
+                <img
+                  src={aboutData.frontmatter.row22Image.publicURL}
+                  className="about-image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column relative text-center">
+          <div className="text-wrapper">
+            <p className="about-title">{aboutData.frontmatter.row2Title}</p>
+            <p className="about-text">{aboutData.frontmatter.row2Text}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -34,13 +61,19 @@ export const query = graphql`
       edges {
         node {
           frontmatter {
-            title
+            row1Title
+            row1Text
+            row2Title
+            row2Text
             path
-            
-            left {
+
+            row1Image {
               publicURL
             }
-            right {
+            row21Image {
+              publicURL
+            }
+            row22Image {
               publicURL
             }
           }
